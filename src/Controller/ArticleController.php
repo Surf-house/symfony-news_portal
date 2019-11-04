c0b38de5445cdcd8d646314b211905b2041a9b98
Surf-house <reeeee@ukr.net>
2019-11-04 18:41:34 +0200

    Create ArticleController

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,12 +2,56 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
-class ArticleController
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Faker\Factory;
+use Faker\Provider\DateTime;
+
+class ArticleController extends AbstractController
 {
 
+    /**
+     * @Route("/article")
+     */
     public function showarticle()
     {
         return new Response('Hello there');
     }
+
+
+
+    /**
+     * @Route("/article/{date}")
+     */
+    
+     public function create($date)
+    {    
+        $faker = Factory::create();
+        
+
+        
+
+        $title = $faker->sentence;
+
+        $text = $faker->text;
+        
+        $date = $faker->date;
+
+        $category = $faker->jobTitle;
+
+    
+
+        return $this->render('article/show.html.twig', [
+            'title'    => $title, 
+            
+            'text'     => $text,
+            'date'     => $date,
+            'category' => $category,
+        ]);
+    }
+
+    
+        
+    
 }
---
+++ src/Controller/ModelController.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Controller;
+
+class ModelController
+{
+    public $category;
+
+    public $title;
+
+    public $body;
+
+    public $date;
+
+    
+}
